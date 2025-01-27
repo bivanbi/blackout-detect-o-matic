bfbb56f51b59ebf59ab9d1a0a98f4ee3cfb57647
Balazs Lengyak <balazs@lengyak.hu>
2025-01-27 07:59:31 +0100

    Set NTP client configuration after object construction

--- lib/NTPClientAdapter/src/NTPClientAdapter.cpp
+++ lib/NTPClientAdapter/src/NTPClientAdapter.cpp
@@ -19,5 +19,25 @@ UnixTimeWithMilliSeconds NTPClientAdapter::getUnixTime() {
     return UnixTimeWithMilliSeconds(ntpClient.getEpochTime());
 }
 
+void NTPClientAdapter::setServerName(const String &serverName) {
+    /**
+     * There is something strange going on in WiFiUDP::beginPacket.
+     * If we pass the String as string.c_str(), the gethostbyname(host) call
+     * in WiFiUDP::beginPacket somehow garbles the host string.
+     * That is why we need to copy the string into a char array.
+     */
+    char *serverNameChar = new char[serverName.length() + 1];
+    serverName.toCharArray(serverNameChar, serverName.length() + 1);
+    ntpClient.setPoolServerName(serverNameChar);
+}
+
+void NTPClientAdapter::setUpdateInterval(unsigned long updateIntervalSeconds) {
+    ntpClient.setUpdateInterval(updateIntervalSeconds);
+}
+
+void NTPClientAdapter::setTimeOffset(int timeOffsetSeconds) {
+    ntpClient.setTimeOffset(timeOffsetSeconds);
+}
+
 WiFiUDP ntpUdp;
-NTPClientAdapter ntpClientAdapter = NTPClientAdapter(NTPClient(ntpUdp, NTP_SERVER, NTP_OFFSET, NTP_UPDATE_INTERVAL));
+NTPClientAdapter ntpClientAdapter = NTPClientAdapter(NTPClient(ntpUdp));
--- lib/NTPClientAdapter/src/NTPClientAdapter.h
+++ lib/NTPClientAdapter/src/NTPClientAdapter.h
@@ -6,7 +6,6 @@
 #include <WiFiUdp.h>
 
 #include "NTPClientAdapter.h"
-#include "SharedSecrets.h"
 #include "UnixTimeWithMilliSeconds.h"
 
 class NTPClientAdapter {
@@ -21,10 +20,17 @@ public:
 
     bool isTimeSet() const;
 
+    void setServerName(const String &serverName);
+
+    void setUpdateInterval(unsigned long updateIntervalSeconds);
+
+    void setTimeOffset(int timeOffsetSeconds);
+
 private:
     NTPClient ntpClient;
 };
 
+extern WiFiUDP ntpUdp;
 extern NTPClientAdapter ntpClientAdapter;
 
 #endif //BLACKOUT_DETECT_O_MATIC_NTP_CLIENT_ADAPTER_H
