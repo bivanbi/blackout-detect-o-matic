8fee103489b1d301a6b4f437138585f72e921e2f
Balazs Lengyak <balazs@lengyak.hu>
2025-02-08 10:24:11 +0100

    Add first draft of app main

---
+++ src/main.cpp
@@ -0,0 +1,80 @@
+#include "main.h"
+
+
+void setup() {
+    serialLogger.info("main::setup(): initial delay to allow serial monitor to connect");
+    delay(3000);
+    serialLogger.info("main::setup(): begin");
+    initConfiguration();
+    initWiFi();
+    initNTP();
+    initSystemStatus();
+    systemStatus.rebootDetected(ntpClientAdapter.getUnixTime());
+    initPins();
+    PeriodicTaskScheduler::init();
+    initTelnetServer();
+    serialLogger.info("main::setup(): done");
+}
+
+void loop() {
+    TelnetServer::loop();
+    PeriodicTaskScheduler::loop();
+}
+
+void initConfiguration() {
+    serialLogger.info("initConfiguration: TODO: load configuration from SD card into temporary object");
+    if (OVERWRITE_CONFIGURATION) {
+        serialLogger.info("initConfiguration: TODO: Update configuration on SD card");
+        // TODO: compare configuration with sdcard content (if available)
+        // TODO: overwrite configuration if needed
+    } else {
+        // TODO: apply configuration from sdcard if available
+        serialLogger.info("initConfiguration: TODO: apply configuration from SD card");
+    }
+
+    serialLogger.info("Configuration: " + configuration.toJsonDocument().as<String>());
+}
+
+void initWiFi() {
+    wifiClient.connect(configuration.getWifiSSID(), configuration.getWifiSecret());
+    while (!wifiClient.isConnected()) {
+        serialLogger.info("Init: Connecting to WiFi network " + String(WIFI_SSID) + ", mac address: " +
+                          String(wifiClient.getMacAddress()));
+        delay(1000);
+    }
+    serialLogger.info("Init: Connected to WiFi network " + String(WIFI_SSID) + ", IP address: " +
+                      String(wifiClient.getIpAddress()));
+}
+
+void initNTP() {
+    serialLogger.info("initNTPClient: Setting NTP server: " + configuration.getNtpServer() + ", update interval: " +
+                      configuration.getNtpUpdateInterval() + " seconds, offset: " + configuration.getNtpOffset() +
+                      " seconds");
+    ntpClientAdapter.setServerName(configuration.getNtpServer());
+    ntpClientAdapter.setUpdateInterval(configuration.getNtpUpdateInterval());
+    ntpClientAdapter.setTimeOffset(configuration.getNtpOffset());
+    PeriodicTaskScheduler::syncTime(true);
+}
+
+void initSystemStatus() {
+    serialLogger.error("initSystemStatus: TODO: load system status from SD card");
+}
+
+void initPins() {
+    PowerDetectorPin::init();
+    ClearAlarmPin::init();
+    ResetSystemStatusPin::init();
+    AlarmLED::init();
+}
+
+void initTelnetServer() {
+    TelnetServer::begin(configuration.getTelnetServerPort());
+}
+
+String getRamInfoString() {
+    // TODO make it a JSON object
+    return "Total heap: " + String(ESP.getHeapSize())
+           + ", tree heap: " + String(ESP.getFreeHeap())
+           + ", total PSRAM: " + String(ESP.getPsramSize())
+           + ", free PSRAM: " + String(ESP.getFreePsram());
+}
---
+++ src/main.h
@@ -0,0 +1,56 @@
+#ifndef BLACKOUT_DETECT_O_MATIC_MAIN_H
+#define BLACKOUT_DETECT_O_MATIC_MAIN_H
+
+#include <Arduino.h>
+#include <random>
+
+#include "Configuration.h"
+#include "SerialLogger.h"
+#include "WifiClient.h"
+#include "NTPClientAdapter.h"
+#include "RTCAdapter.h"
+#include "SystemStatus.h"
+#include "PowerDetectorPin.h"
+#include "PowerChangeEventProcessor.h"
+#include "ClearAlarmPin.h"
+#include "ClearAlarmEventProcessor.h"
+#include "ResetSystemStatusPin.h"
+#include "ResetSystemStatusEventProcessor.h"
+#include "AlarmLED.h"
+#include "TelnetServer.h"
+#include "PeriodicTaskScheduler.h"
+
+#define LOG_FILEPATH_FORMAT LOG_DIRECTORY + "/log_%Y-%m-%d.log";
+#define SYSTEM_STATUS_FILEPATH "/system_status.json"
+
+void initConfiguration();
+
+void initWiFi();
+
+void initNTP();
+
+void initSystemStatus();
+
+void initPins();
+
+void initTimer();
+
+void initTelnetServer();
+
+void executePeriodicTasks();
+
+void updateSystemStatus();
+
+void processEvents();
+
+void syncTime();
+
+void onPeriodicTaskTimerInterrupt();
+
+String logRamInfoString();
+
+String getHeartBeatMessage();
+
+Configuration loadConfiguration();
+
+#endif //BLACKOUT_DETECT_O_MATIC_MAIN_H
